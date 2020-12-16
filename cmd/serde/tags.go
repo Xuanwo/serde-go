cd19df188b8ce1e6822b6e1d111c188b51b34816
Xuanwo <github@xuanwo.io>
2020-12-16 18:05:47 +0800

    cmd: Add struct skip tag support
    Signed-off-by: Xuanwo <github@xuanwo.io>

---
+++ cmd/serde/tags.go
@@ -0,0 +1,12 @@
+package main
+
+const (
+	// Container tags
+	TagDeserialize = "deserialize"
+	TagSerialize   = "serialize"
+
+	// Field tags
+	TagSkip            = "skip"
+	TagSkipSerialize   = "skip_serialize"
+	TagSkipDeserialize = "skip_deserialize"
+)
